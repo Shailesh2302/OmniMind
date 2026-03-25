97c70b4c8e8d2e478ce6f35c5434e3ee80440498
Rushikesh kanfade <rushikeshkanfade123@gmail.com>
2026-03-25 10:10:29 +0530

    Initializing the project

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { Card } from "@repo/ui/card";
+import { Gradient } from "@repo/ui/gradient";
+import { TurborepoLogo } from "@repo/ui/turborepo-logo";
+
+
+export default async function Page() {
+
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+     <h6>Welcome to the jumanjee</h6>
+    </main>
+  );
+}
